84f160f814b561c864c80409c7d8868fb290c913
Sathish <mail2sathish11@gmail.com>
2022-10-22 22:26:31 +0530

    updated tests

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,5 +1,5 @@
 import pytest
-from src.utils import read_yaml
+from src.utils import Helper
 from pathlib import Path
 from box import ConfigBox
 from ensure.main import EnsureError
@@ -10,13 +10,13 @@ class Test_read_yaml:
 
     def test_read_yaml_empty(self):
         with pytest.raises(ValueError):
-            read_yaml(Path(self.yaml_files[0]))
+            Helper.read_yaml(Path(self.yaml_files[0]))
 
     def test_read_yaml_return_type(self):
-        respone = read_yaml(Path(self.yaml_files[-1]))
+        respone = Helper.read_yaml(Path(self.yaml_files[-1]))
         assert isinstance(respone, ConfigBox)
 
     @pytest.mark.parametrize("path_to_yaml", yaml_files)
     def test_read_yaml_bad_type(self, path_to_yaml):
         with pytest.raises(EnsureError):
-            read_yaml(path_to_yaml)
+            Helper.read_yaml(path_to_yaml)
